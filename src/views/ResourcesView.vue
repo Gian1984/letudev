7ed73d42071f99d60b4f1cb05e7a48db27d49355
Gianluca <gl.tiengo@gmail.com>
2022-04-09 16:31:44 +0200

    adding gategories

--- src/views/ResourcesView.vue
+++ src/views/ResourcesView.vue
@@ -1,18 +1,18 @@
 <template>
   <main class="-mt-32 max-w-7xl mx-auto relative z-10 px-4 sm:px-6 lg:px-8 mt-24 mb-10">
     <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
-      <div v-for="person in people" :key="person.email" class="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-teal-500">
+      <div v-for="resource in resources" :key="resource.name" class="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-teal-500">
         <div class="flex-shrink-0">
-          <img class="h-10 w-10 rounded-full" :src="person.imageUrl" alt="" />
+          <img class="h-10 w-10 rounded-full" :src="resource.imageUrl" alt="" />
         </div>
         <div class="flex-1 min-w-0">
-          <router-link to="#" class="focus:outline-none">
+          <router-link :to="resource.href" class="focus:outline-none">
             <span class="absolute inset-0" aria-hidden="true" />
             <p class="text-sm font-medium text-gray-900">
-              {{ person.name }}
+              {{ resource.name }}
             </p>
             <p class="text-sm text-gray-500 truncate">
-              {{ person.role }}
+              {{ resource.desc }}
             </p>
           </router-link>
         </div>
@@ -22,184 +22,221 @@
 </template>
 
 <script>
-const people = [
+const resources = [
   {
     name: 'UI Graphics',
-    href:'',
+    href:'/uigraphics',
+    desc:'Websites and resources with modern UI components in different formats such as PSD, Sketch, Figma, etc. They are great for ideas for web components/UI.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Fonts',
-    href:'',
+    href:'/fonts',
+    desc:'Websites that offer free fonts as well as font-based tools.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Colors',
-    href:'',
+    href:'/colors',
+    desc:'Websites and resources that help with choices related to colors.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Icons',
-    href:'',
+    href:'/icons',
+    desc:'Resources for Icons including png, svg and more.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Logos',
-    href:'',
+    href:'/logos',
+    desc:'Resources Logos',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Favicons',
-    href:'',
+    href:'/favicons',
+    desc:'Resources Favicons.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Icon Fonts',
-    href:'',
+    href:'/iconfonts',
+    desc:Resources Icon Fonts,
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Stock Photos',
-    href:'',
+    href:'/stockphotos',
+    desc:'Websites that offer free stock photos of all kinds for your websites and apps.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Stock Videos',
-    href:'',
+    href:'/stockvideos',
+    desc:'Websites that offer free stock videos of all kinds for your websites and apps.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Stock Music & Sound Effects',
-    href:'',
+    href:'/soundeffects',
+    desc:'Websites that offer free stock music and/or sound effects.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Vectors & Clip Art',
-    href:'',
+    href:'/vectorsclip',
+    desc:'Free vectors, clipart, illustrations, patterns and more.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Product & Image Mockups',
-    href:'',
+    href:'/imagemockups',
+    desc:'Create mockups of devices and other products.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'HTML & CSS Templates',
-    href:'',
+    href:'/html&csstemplates',
+    desc:'Websites that offer free beautiful website templates and themes of all types.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'CSS Frameworks',
-    href:'',
+    href:'/cssframeworks',
+    desc:'CSS/UI frameworks to help build great looking websites and applications.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'CSS Methodologies',
-    href:'',
+    href:'/cssmethodologies',
+    desc:'CSS methodologies to help write modular, reusable and scalable code.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'CSS Animations',
-    href:'',
+    href:'/cssanimations',
+    desc:'CSS animations to build awesome animations for websites and applications.'
+    imageUrl:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Javascript Animation Libraries',
+    href:'/animationlibraries',
+    desc:'Javascript animations libraries to build awesome animations for websites and applications.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Javascript Chart Libraries',
-    href:'',
+    href:'/chartlibraries',
+    desc:'Libraries that help developers visualize data into charts.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'UI Components & Kits',
-    href:'',
+    href:'/uicomponents',
+    desc:'Not quite "frameworks", but tools for creating user interfaces with components or UI kits',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'React UI Libraries',
-    href:'',
+    href:'/reactuilibraries',
+    desc:'UI and component libraries for the React JavaScript framework.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Vue UI Libraries',
-    href:'',
+    href:'/vueuilibraries',
+    desc:'UI and component libraries for the Vue JavaScript framework.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Angular UI Libraries',
-    href:'',
+    href:'/angularuilibraries',
+    desc:'UI and component libraries for the Angular JavaScript framework.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Svelte UI Libraries',
-    href:'',
+    href:'/svelteuilibraries',
+    desc:'UI and component libraries for the Svelte JavaScript compiler.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'React Native UI Libraries',
-    href:'',
+    href:'/reactnativeuilibraries',
+    desc:'UI and component libraries for the React Native Framework.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Design Systems & Style Guides',
-    href:'',
+    href:'/designsystems',
+    desc:'Design systems, style guides, toolkits, docs. Some of these are design guides for top companies/websites.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Online Design Tools',
-    href:'',
+    href:'/onlinedesigntools',
+    desc:'All kinds of online tools for design, from photo editors to wireframing, and more.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Downloadable Design Software',
-    href:'',
+    href:'/designsoftware',
+    desc:'Free software for UI, photo, 3d modeling, etc. Alternatives to paid software like Photoshop.'
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Design Inspiration',
-    href:'',
+    href:'/designinspiration',
+    desc:'Here are some websites to get inspiration for design and UI.,
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Image Compression',
-    href:'',
+    href:'/imagecompression',
+    desc:'Websites that allow you to compress large images.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Chrome Extensions',
-    href:'',
+    href:'/chromeextensions',
+    desc:'Useful Chrome extensions for Designers and Web-Developers.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
     name: 'Others',
-    href:'',
+    href:'/others',
+    desc:'Uncategorized Stuff.',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
@@ -209,7 +246,7 @@ const people = [
 export default {
   setup() {
     return {
-      people,
+      resources,
     }
   },
 }
