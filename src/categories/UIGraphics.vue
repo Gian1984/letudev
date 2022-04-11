a0d25475b274a81f77c815a221b953b1207efd14
Gianluca <gl.tiengo@gmail.com>
2022-04-11 08:43:00 +0200

    working on navigation

--- src/categories/UIGraphics.vue
+++ src/categories/UIGraphics.vue
@@ -1,4 +1,54 @@
 <template>
+  <header>
+    <div>
+      <div>
+        <img class="h-32 w-full object-cover lg:h-48" src="'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'," alt="header_res" />
+      </div>
+      <div class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div>
+          <div>
+            <nav class="sm:hidden" aria-label="Back">
+              <router-link to="/resources" class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700">
+                <ChevronLeftIcon class="flex-shrink-0 -ml-1 mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+                Resources
+              </router-link>
+            </nav>
+            <nav class="hidden sm:flex" aria-label="Breadcrumb">
+              <ol role="list" class="flex items-center space-x-4">
+                <li>
+                  <div class="flex">
+                    <router-link to="/" class="text-sm font-medium text-gray-500 hover:text-gray-700">Home</router-link>
+                  </div>
+                </li>
+                <li>
+                  <div class="flex items-center">
+                    <ChevronRightIcon class="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
+                    <router-link to="/resources" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">Resources</router-link>
+                  </div>
+                </li>
+                <li>
+                  <div class="flex items-center">
+                    <ChevronRightIcon class="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
+                    <router-link to="/uigraphics" aria-current="page" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">UI Graphics</router-link>
+                  </div>
+                </li>
+              </ol>
+            </nav>
+          </div>
+          <div class="mt-2 md:flex md:items-center md:justify-between">
+            <div class="flex-1 min-w-0">
+              <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">UI Graphics</h2>
+            </div>
+<!--            <div class="mt-4 flex-shrink-0 flex md:mt-0 md:ml-4">-->
+<!--              <button type="button" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Edit</button>-->
+<!--              <button type="button" class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Publish</button>-->
+<!--            </div>-->
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
   <main class="-mt-32 max-w-7xl mx-auto relative z-10 px-4 sm:px-6 lg:px-8 mt-24 mb-10">
     <h2 class="text-gray-500 text-xs font-medium uppercase tracking-wide">Pinned Projects</h2>
     <ul role="list" class="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-1 lg:grid-cols-2">
@@ -6,7 +56,7 @@
         <div :class="[project.bgColor, 'flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md']"></div>
         <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
           <div class="flex-1 px-4 py-2 text-sm truncate">
-            <a :href="project.href" class="text-gray-900 font-medium hover:text-gray-600 uppercase">{{ project.name }}</a>
+            <a :href="project.href" target="_blank" class="text-gray-900 font-medium hover:text-gray-600 uppercase">{{ project.name }}</a>
             <p class="text-gray-500">{{ project.desc }} </p>
           </div>
           <div class="flex-shrink-0 pr-2">
@@ -23,6 +73,7 @@
 
 <script>
 import { ChevronDoubleRightIcon } from '@heroicons/vue/outline'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/vue/solid'
 
 const projects = [
   { name: 'UI Design Daily', href: 'https://uidesigndaily.com/', desc:'Awesome UI Components of all types', bgColor: 'bg-pink-600' },
@@ -71,12 +122,13 @@ const projects = [
   { name: 'HOLA SVG!', href: "https://holasvg.com/", desc: 'An SVG playground to share SVG stuff', bgColor: 'bg-pink-500' },
   { name: 'Symu.co', href:"https://symu.co/freebies/templates-4/", desc:'Free templates, UI kits, icon, PSD', bgColor: 'bg-purple-600' },
   { name: 'Mesh Gradient', href: "https://meshgradient.in/", desc: 'Generate & download beautiful mesh gradients', bgColor: 'bg-yellow-500' },
-
 ]
 
 export default {
   components: {
     ChevronDoubleRightIcon,
+    ChevronLeftIcon,
+    ChevronRightIcon
   },
   setup() {
     return {
